747b5bafcd947cc2c5d86ea2ccf4bb5b767965c0
kemorami <is.go.lk777.org0@gmail.com>
2024-02-11 11:23:58 +0300

    file

--- src/components/content/Post/Post.jsx
+++ src/components/content/Post/Post.jsx
@@ -1,11 +1,16 @@
 import img from '../../../img/ava.jpg'
 import styles from './Post.module.css'
- function Post({name="Имя",text="Сообщение",img}){
+import like from '../../../img/pngwing.com.png'
+ function Post({name="Имя",text="Сообщение",img,numberLikes="1"}){
     return(
             <div className={styles.post}>
                 <img src={img} alt="avatar" className={styles.avatar}/>
                 <p className={styles.postName}>{name}</p>
                 <p>{text}</p>
+                <div className={styles.likes}>
+                    <img src={like} alt="" className={styles.like}/>
+                    <p className={styles.likeNumber}>{numberLikes}</p>
+                </div>
             </div>
     )
  }
--- src/components/content/posts/Posts.jsx
+++ src/components/content/posts/Posts.jsx
@@ -3,13 +3,13 @@ import Post from '../Post/Post.jsx'
 import img from '../../../img/fenics.jpg'
 import imgg from '../../../img/i.webp'
 import immg from '../../../img/ava.jpg'
-let postsUser = [{name:"Алексей", text:"Всем привет", img:img},{name:"Кирилл", text:"Да", img:imgg},{name:"Матвей", text:"Нет", img:immg}]
+let postsUser = [{name:"Алексей", text:"Всем привет", img:img, numberLikes:"10"},{name:"Кирилл", text:"Да", img:imgg, numberLikes:"5"},{name:"Матвей", text:"Нет", img:immg, numberLikes:"129"}]
 function Posts(){
     return(
         <div className={styles.posts}>
             <input type="text" className={styles.input}/>
             <button className={styles.button}>Добавить пост</button>
-            {postsUser.map((item,idx)=> <Post name={item.name} text={item.text} img={item.img} key={idx}/>)}
+            {postsUser.map((item,idx)=> <Post name={item.name} text={item.text} img={item.img} numberLikes={item.numberLikes} key={idx}/>)}
         </div>
     )
 }
